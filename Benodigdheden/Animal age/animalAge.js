591315f03230d2a0444ebe65c486987515f99fe3
Esperitz <88886@glr.nl>
2023-03-06 15:31:13 +0100

    graagh!

---
+++ Benodigdheden/Animal age/animalAge.js
@@ -0,0 +1,5 @@
+var dogAge = 5.5;
+var geckoAge = 11;
+var elephantAge = 1.6;
+var flyAge = 1216;
+
--- Benodigdheden/Enigma [eindopdracht]/enigma.js
+++ Benodigdheden/Enigma [eindopdracht]/enigma.js
@@ -1,6 +1,6 @@
 function encrypt() {
     let message = document.getElementById("message").value;
-    let shift = 3; // number of places to shift each letter
+    let cipher = 3;
     let encryptedMessage = "";
   
     for (let i = 0; i < message.length; i++) {
@@ -10,9 +10,9 @@ function encrypt() {
         let code = message.charCodeAt(i);
   
         if (code >= 65 && code <= 90) {
-          char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+          char = String.fromCharCode(((code - 65 + cipher) % 26) + 65);
         } else if (code >= 97 && code <= 122) {
-          char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+          char = String.fromCharCode(((code - 97 + cipher) % 26) + 97);
         }
       }
   
